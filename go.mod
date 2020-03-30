7846c069320836fe6fe85ee88b9d4fde0dd59750
tbruggeman <tom.bruggeman@persgroep.be>
2020-03-30 15:14:42 +0200

    Added table output to integration_test.go

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/joho/godotenv v1.3.0
+	github.com/olekukonko/tablewriter v0.0.4
 	github.com/stretchr/testify v1.5.1
 	gopkg.in/h2non/gock.v1 v1.0.15
 )
--- integration_test.go
+++ integration_test.go
@@ -4,20 +4,23 @@ package pandascore
 
 import (
 	"log"
+	"os"
+	"strconv"
 	"testing"
 
 	"github.com/joho/godotenv"
+	"github.com/olekukonko/tablewriter"
 	"github.com/stretchr/testify/assert"
 )
 
 func loadEnvironmentVariables() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Must have .env file with environment variables in project root")
+		log.Fatal("Missing .env file with " + AccessTokenEnvironmentVariable + " variable in project root")
 	}
 }
 
-func TestIntegration_getLeagues(t *testing.T) {
+func TestIntegration_getCSGOLeagues(t *testing.T) {
 	loadEnvironmentVariables()
 
 	leagues, err := New().GetLeagues(CSGO)
@@ -27,4 +30,15 @@ func TestIntegration_getLeagues(t *testing.T) {
 	assert.NotEmpty(t, leagues[0].ID)
 	assert.NotEmpty(t, leagues[0].Name)
 	assert.NotEmpty(t, leagues[0].Modified)
+
+	outputLeaguesAsTable(leagues)
+}
+
+func outputLeaguesAsTable(leagues []League) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"ID", "Name", "Modified"})
+	for _, league := range leagues {
+		table.Append([]string{strconv.Itoa(league.ID), league.Name, league.Modified.String()})
+	}
+	table.Render()
 }
