135c1f164f6ceee4bf8dfb20477c849ff85eb62f
tbruggeman <tom.bruggeman@persgroep.be>
2020-04-04 18:12:05 +0200

    Removed duplicate Series test in matches_test.go

--- matches_test.go
+++ matches_test.go
@@ -26,32 +26,3 @@ func TestClient_GetAllUpcomingMatches(t *testing.T) {
 	assert.IsType(t, []Match{}, result)
 	assert.Len(t, result, 3)
 }
-
-func TestClient_GetAllUpcomingMatches_withPaging(t *testing.T) {
-	defer gock.Off()
-	defer assert.True(t, gock.IsDone())
-
-	gock.New("https://api.pandascore.co/csgo/series/running").
-		Reply(http.StatusOK).
-		AddHeader("X-Page", "1").
-		AddHeader("X-Per-Page", "2").
-		AddHeader("X-Total", "4").
-		File("testdata/csgo-series-running.json")
-
-	gock.New("https://api.pandascore.co/csgo/series/running").
-		MatchParam("page", "2").
-		Reply(http.StatusOK).
-		AddHeader("X-Page", "2").
-		AddHeader("X-Per-Page", "2").
-		AddHeader("X-Total", "4").
-		File("testdata/csgo-series-running2.json")
-
-	client := New()
-	result, err := client.GetAllRunningSeries(CSGO)
-
-	assert.Nil(t, err)
-	assert.NotNil(t, result)
-	assert.IsType(t, []Series{}, result)
-	assert.Len(t, result, 4)
-	assert.Equal(t, []int{2522, 2528, 2523, 2529}, []int{result[0].ID, result[1].ID, result[2].ID, result[3].ID})
-}
