769bd44ff392f316b7651ddc1fd4d6194cd7b867
tbruggeman <tom.bruggeman@persgroep.be>
2020-04-30 18:03:00 +0200

    Added LoL support

--- games.go
+++ games.go
@@ -3,6 +3,7 @@ package pandascore
 const (
 	CSGO  Game = "csgo"
 	Dota2 Game = "dota2"
+	LoL   Game = "lol"
 )
 
 // Game represents a single game in the PandaScore API (eg. csgo, dota2, ...)
@@ -10,5 +11,5 @@ type Game string
 
 // Validates that the access token is valid.
 func (g Game) IsValid() bool {
-	return g == CSGO || g == Dota2
+	return g == CSGO || g == Dota2 || g == LoL
 }
--- games_test.go
+++ games_test.go
@@ -9,5 +9,6 @@ import (
 func TestGame_IsValid(t *testing.T) {
 	assert.True(t, CSGO.IsValid())
 	assert.True(t, Dota2.IsValid())
+	assert.True(t, LoL.IsValid())
 	assert.False(t, Game("doesn't exist").IsValid())
 }
